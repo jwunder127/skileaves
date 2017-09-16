f74e67473beff311d5f9fc123eb314241635eec1
John Underwood <john.w.underwood.iii@gmail.com>
2017-09-16 18:34:48 -0400

    Update webpack config

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 class App extends React.Component {
 
 render() {
-    console.log('this should show up');
     return (
      <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
+        <h1>Hello World!!!</h1>
       </div>);
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,51 @@
+'use strict';
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './public/index.html',
-  filename: 'index.html',
-  inject: 'body'
-});
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+const devMode = process.env.NODE_ENV === 'development';
 
+/**
+ * Fast source maps rebuild quickly during development, but only give a link
+ * to the line where the error occurred. The stack trace will show the bundled
+ * code, not the original code. Keep this on `false` for slower builds but
+ * usable stack traces. Set to `true` if you want to speed up development.
+ */
 
-module.exports = {
+const USE_FAST_SOURCE_MAPS = false;
+
+const config = {
   entry: './src/main.js',
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
   context: __dirname,
-  devtool: 'source-map',
+  devtool: devMode && USE_FAST_SOURCE_MAPS
+    ? 'cheap-module-eval-source-map'
+    : 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '*']
   },
   module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+    rules: [{
+      test: /jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader' // config in .babelrc
+    }]
   },
-//   plugins: [HtmlWebpackPluginConfig]
-
+  plugins: devMode
+    ? [new LiveReloadPlugin({appendScriptTag: true})]
+    : []
 };
+
+if (devMode){
+  config.devtool = USE_FAST_SOURCE_MAPS
+    ? 'cheap-module-eval-source-map'
+    : 'source-map';
+  config.plugins.push(
+    new LiveReloadPlugin({
+      appendScriptTag: true
+    })
+  );
+}
+
+module.exports = config;
